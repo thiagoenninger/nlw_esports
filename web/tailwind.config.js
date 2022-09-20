dd609ac74aa9ba82f1d30213b586fa39358bb2d6
Thiago Vazquez Enninger <thiagoenninger@gmail.com>
2022-09-20 09:57:12 -0300

    página web quase finalizada

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -5,10 +5,14 @@ module.exports = {
     './index.html',
   ],
   theme: {
+    fontFamily:{
+      sans:['Inter', 'sans-serif'],
+    },
     extend: {
       backgroundImage:{
         galaxy: "url('/background-galaxy.png')",
         'gradient-duo': 'linear-gradient(60deg, #9572FC 21%, #43E7AD 54%, #E1D55D 70%)',
+        'game-text-gradient': 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67%)',
       }
     },
   },
