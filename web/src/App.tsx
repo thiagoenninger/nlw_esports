ea7d01bc6ed1c2c5254b921ef918cb49e6db8553
Thiago Vazquez Enninger <thiagoenninger@gmail.com>
2022-09-20 08:49:55 -0300

    adicionado fundo padrao no html, a logo e imagens dos games em react, mudanca de pasta dos imagens para public

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,36 @@
 import './styles/main.css';
 
+import logoImg from './assets/logo-esports.svg';
+
 function App() {
   return (
-    <div>
+    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
+      <img src={logoImg} alt="" />
 
+      <h1 className='text-6xl text-white font-black mt-20'>
+        SEU <span className="bg-gradient-duo text-transparent bg-clip-text">DUO</span> ESTÁ AQUI
+      </h1>
+
+      <div className="grid grid-cols-6 gap-6 my-16">
+        <a href="">
+          <img src="/game-1.png" alt="" />
+        </a>
+        <a href="">
+          <img src="/game-2.png" alt="" />
+        </a>
+        <a href="">
+          <img src="/game-3.png" alt="" />
+        </a>
+        <a href="">
+          <img src="/game-4.png" alt="" />
+        </a>
+        <a href="">
+          <img src="/game-5.png" alt="" />
+        </a>
+        <a href="">
+          <img src="/game-6.png" alt="" />
+        </a>
+      </div>
     </div>
   )
 }
--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -2,9 +2,15 @@
 module.exports = {
   content: [
     './src/**/*.tsx',
+    './index.html',
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage:{
+        galaxy: "url('/background-galaxy.png')",
+        'gradient-duo': 'linear-gradient(60deg, #9572FC 21%, #43E7AD 54%, #E1D55D 70%)',
+      }
+    },
   },
   plugins: [],
 }
