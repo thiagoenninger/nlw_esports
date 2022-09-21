23ffc3187cbe5aeb77c51e76567c34a37d15d81f
Thiago Vazquez Enninger <thiagoenninger@gmail.com>
2022-09-21 08:54:35 -0300

    criação do README backend e adicionado os métodos HTTP

---
+++ server/README.md
@@ -0,0 +1,26 @@
+# Back-end
+
+## Entidades
+
+  ### Game
+    id
+    title
+    bannerUrl
+
+  ### Ad
+    id
+    gameId
+    name
+    yearsPlaying
+    discord
+    weekDays
+    hourStart
+    hourEnd
+    useVoiceChannel
+    createAt  
+
+## Casos de uso
+  - Listagem de games com contagem de anúncios;
+  - Criação de novo anúncio;
+  - Listagem de anúncios por game;
+  - Buscar discord pelo Id do anúncio.
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,40 @@
-import express from 'express';
+import express, { response } from 'express';
 
 const app = express();
 
-app.get('/ads', (request, response) => {
+/**
+  *Parametros:
+    - Query: (?) -> Persistir estado (manter o estado atual da página)
+    - Route: identificadores de recursos
+    - Body: enviar várias informações em uma requisição (form por exemplo)
+ */
+
+// HTTP methods / API RESTful
+  // => GET, POST, PUT, PATCH, DELETE
+  // HTTP Codes => 2xx (sucesso), 3xx(redirecionamento), 4xx (erro da aplicação), 5xx (erros inesperados)
+
+app.get('/games', (request, response) => {
+  return response.json([]);
+});
+
+app.post('/ads', (request, response) => {
+  return response.status(201).json([]);
+}); 
+
+app.get('/games/:id/ads', (request, response) => {
+  // const adId = request.params.id;
+
+  return response.json([
+    {id:1, name: 'Anúncio 1'},
+    {id:2, name: 'Anúncio 2'},
+    {id:3, name: 'Anúncio 3'},
+    {id:4, name: 'Anúncio 4'}
+  ])
+})
+
+app.get('/ads/:id/discord', (request, response) => {
+  // const gameId = request.params.id;
+
   return response.json([
     {id:1, name: 'Anúncio 1'},
     {id:2, name: 'Anúncio 2'},
